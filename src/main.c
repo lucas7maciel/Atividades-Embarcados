631d6f149c6cea79f4f8c0e2f540e090e19d0de4
Lucas Maciel <lucasmacielcontato@gmail.com>
2025-11-03 00:27:29 -0300

    first commit

--- src/main.c
+++ src/main.c
@@ -1,227 +1,157 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/util.h>
+#include <zephyr/net/sntp.h>
+#include <zephyr/sys/timeutil.h>
+#include <zephyr/zbus/zbus.h>
+
+#include <time.h>
+#include <string.h>
 
 LOG_MODULE_REGISTER(app, LOG_LEVEL_INF);
 
-enum sensor_type {
-	SENSOR_TEMPERATURE = 0,
-	SENSOR_HUMIDITY,
-	SENSOR_TYPE_COUNT
+struct time_message {
+	uint64_t epoch_seconds;
+	uint16_t milliseconds;
+	int64_t epoch_ms;
+	struct tm utc_time;
 };
 
-enum sensor_error {
-	SENSOR_ERR_TOO_LOW = 0,
-	SENSOR_ERR_TOO_HIGH
-};
+ZBUS_MSG_SUBSCRIBER_DEFINE(logger_sub);
+ZBUS_MSG_SUBSCRIBER_DEFINE(app_sub);
 
-struct sensor_reading {
-	enum sensor_type type;
-	int16_t value;
-	int64_t timestamp_ms;
-};
+ZBUS_CHAN_DEFINE(time_channel, struct time_message, NULL, NULL,
+		 ZBUS_OBSERVERS(logger_sub, app_sub),
+		 ZBUS_MSG_INIT(.epoch_seconds = 0, .milliseconds = 0,
+			       .epoch_ms = 0));
 
-struct sensor_fault {
-	struct sensor_reading sample;
-	enum sensor_error error;
-	int16_t min_expected;
-	int16_t max_expected;
-};
+#define SNTP_STACK_SIZE 2048
+#define LISTENER_STACK_SIZE 2048
+#define APPLICATION_STACK_SIZE 2048
 
-/* Buffer das filas (cada item é copiado para essa região estática) */
-#define QUEUE_ALIGNMENT 4
+#define SNTP_THREAD_PRIORITY 4
+#define LOGGER_THREAD_PRIORITY 5
+#define APPLICATION_THREAD_PRIORITY 5
 
-K_MSGQ_DEFINE(input_queue, sizeof(struct sensor_reading),
-	      CONFIG_FILTER_QUEUE_DEPTH, QUEUE_ALIGNMENT);
-K_MSGQ_DEFINE(output_queue, sizeof(struct sensor_reading),
-	      CONFIG_FILTER_QUEUE_DEPTH, QUEUE_ALIGNMENT);
-K_MSGQ_DEFINE(error_queue, sizeof(struct sensor_fault),
-	      CONFIG_FILTER_QUEUE_DEPTH, QUEUE_ALIGNMENT);
-
-#define PRODUCER_STACK_SIZE 1024
-#define FILTER_STACK_SIZE 1024
-#define CONSUMER_STACK_SIZE 1024
-#define LOGGER_STACK_SIZE 1024
-
-#define PRODUCER_PRIORITY 5
-#define FILTER_PRIORITY 4
-#define CONSUMER_PRIORITY 6
-#define LOGGER_PRIORITY 6
-
-static const int16_t temperature_pattern[] = { 21, 17, 19, 35, 28, 14, 30, 26 };
-static const int16_t humidity_pattern[] = { 45, 32, 55, 68, 72, 60, 38, 66 };
-
-static const char *const sensor_names[] = {
-	[SENSOR_TEMPERATURE] = "Temperatura",
-	[SENSOR_HUMIDITY] = "Umidade",
-};
-
-static void producer_thread(void *pattern_ptr, void *type_ptr, void *unused)
+static uint16_t fractional_to_ms(uint32_t fraction)
 {
-	const int16_t *pattern = pattern_ptr;
-	const enum sensor_type type = POINTER_TO_INT(type_ptr);
-	const size_t pattern_len =
-		(type == SENSOR_TEMPERATURE) ? ARRAY_SIZE(temperature_pattern) :
-					       ARRAY_SIZE(humidity_pattern);
-	size_t index = 0;
-	const uint32_t sleep_ms = (type == SENSOR_TEMPERATURE) ?
-				      CONFIG_TEMP_PRODUCER_INTERVAL_MS :
-				      CONFIG_HUM_PRODUCER_INTERVAL_MS;
+	return (uint16_t)(((uint64_t)fraction * 1000U) >> 32);
+}
 
-	ARG_UNUSED(unused);
+static void fill_utc_time(uint64_t epoch_seconds, struct tm *out_tm)
+{
+	const time_t seconds32 =
+		(time_t)MIN(epoch_seconds, (uint64_t)SYS_TIME_T_MAX);
+
+	if (gmtime_r(&seconds32, out_tm) == NULL) {
+		memset(out_tm, 0, sizeof(*out_tm));
+	}
+}
+
+static void sntp_client_thread(void *, void *, void *)
+{
+	const uint16_t port = CONFIG_SNTP_SERVER_PORT;
+	const uint32_t interval_ms =
+		(uint32_t)CONFIG_SNTP_UPDATE_INTERVAL_SEC * 1000U;
+
+	k_thread_name_set(k_current_get(), "sntp-client");
+
+	/* Aguarda assinantes submeterem-se ao canal */
+	k_sleep(K_SECONDS(1));
 
 	while (true) {
-		struct sensor_reading sample = {
-			.type = type,
-			.value = pattern[index],
-			.timestamp_ms = k_uptime_get(),
-		};
-
-		int ret = k_msgq_put(&input_queue, &sample, K_FOREVER);
+		struct sntp_time sntp_time = { 0 };
+		int ret = sntp_simple(CONFIG_SNTP_SERVER_HOSTNAME, port,
+				      &sntp_time);
 
 		if (ret == 0) {
-			LOG_DBG("%s produzida: %d",
-				sensor_names[sample.type], sample.value);
-		} else {
-			LOG_ERR("Falha ao enfileirar amostra (%d)", ret);
-		}
-
-		index = (index + 1U) % pattern_len;
-
-		k_msleep(sleep_ms);
-	}
-}
-
-static bool validate_reading(const struct sensor_reading *sample,
-			     int16_t *min_allowed, int16_t *max_allowed,
-			     enum sensor_error *error)
-{
-	switch (sample->type) {
-	case SENSOR_TEMPERATURE:
-		*min_allowed = CONFIG_TEMP_VALID_MIN_C;
-		*max_allowed = CONFIG_TEMP_VALID_MAX_C;
-		break;
-	case SENSOR_HUMIDITY:
-		*min_allowed = CONFIG_HUM_VALID_MIN_PCT;
-		*max_allowed = CONFIG_HUM_VALID_MAX_PCT;
-		break;
-	default:
-		/* Não deveria ocorrer */
-		*min_allowed = INT16_MIN;
-		*max_allowed = INT16_MAX;
-		break;
-	}
-
-	if (sample->value < *min_allowed) {
-		*error = SENSOR_ERR_TOO_LOW;
-		return false;
-	}
-
-	if (sample->value > *max_allowed) {
-		*error = SENSOR_ERR_TOO_HIGH;
-		return false;
-	}
-
-	return true;
-}
-
-static void filter_thread(void *, void *, void *)
-{
-	while (true) {
-		struct sensor_reading sample;
-		int ret = k_msgq_get(&input_queue, &sample, K_FOREVER);
-
-		if (ret != 0) {
-			LOG_ERR("Falha ao ler fila de entrada (%d)", ret);
-			continue;
-		}
-
-		int16_t min_allowed = 0;
-		int16_t max_allowed = 0;
-		enum sensor_error error_flag = SENSOR_ERR_TOO_LOW;
-
-		const bool valid = validate_reading(&sample, &min_allowed,
-						    &max_allowed, &error_flag);
-
-		if (valid) {
-			ret = k_msgq_put(&output_queue, &sample, K_FOREVER);
-			if (ret != 0) {
-				LOG_ERR("Falha ao enviar dado válido (%d)", ret);
-			}
-		} else {
-			struct sensor_fault fault = {
-				.sample = sample,
-				.error = error_flag,
-				.min_expected = min_allowed,
-				.max_expected = max_allowed,
+			struct time_message msg = {
+				.epoch_seconds = sntp_time.seconds,
+				.milliseconds =
+					fractional_to_ms(sntp_time.fraction),
 			};
 
-			ret = k_msgq_put(&error_queue, &fault, K_FOREVER);
+			msg.epoch_ms =
+				(int64_t)msg.epoch_seconds * 1000LL +
+				msg.milliseconds;
+			fill_utc_time(msg.epoch_seconds, &msg.utc_time);
+
+			LOG_INF("SNTP sincronizado: %s:%u -> %lld ms",
+				CONFIG_SNTP_SERVER_HOSTNAME, port,
+				msg.epoch_ms);
+
+			ret = zbus_chan_pub(&time_channel, &msg, K_SECONDS(1));
 			if (ret != 0) {
-				LOG_ERR("Falha ao enviar dado inválido (%d)",
-					ret);
+				LOG_ERR("Falha ao publicar tempo (%d)", ret);
 			}
+		} else {
+			LOG_WRN("SNTP falhou (%d), mantendo ultimo horario",
+				ret);
 		}
+
+		k_msleep(interval_ms);
 	}
 }
 
-static void consumer_thread(void *, void *, void *)
+static void logger_thread(void *, void *, void *)
 {
-	while (true) {
-		struct sensor_reading sample;
-		int ret = k_msgq_get(&output_queue, &sample, K_FOREVER);
+	const struct zbus_observer *sub = &logger_sub;
+	const struct zbus_channel *chan = NULL;
+	struct time_message msg;
 
-		if (ret != 0) {
-			LOG_ERR("Falha ao ler fila de saída (%d)", ret);
+	k_thread_name_set(k_current_get(), "logger");
+
+	while (zbus_sub_wait_msg(sub, &chan, &msg, K_FOREVER) == 0) {
+		if (chan != &time_channel) {
 			continue;
 		}
 
-		LOG_INF("Consumidor recebeu %s = %d (t=%lld ms)",
-			sensor_names[sample.type], sample.value,
-			sample.timestamp_ms);
+		LOG_INF("[Logger] %04d-%02d-%02d %02d:%02d:%02d.%03u UTC "
+			"(epoch_ms=%lld)",
+			msg.utc_time.tm_year + 1900, msg.utc_time.tm_mon + 1,
+			msg.utc_time.tm_mday, msg.utc_time.tm_hour,
+			msg.utc_time.tm_min, msg.utc_time.tm_sec,
+			msg.milliseconds, msg.epoch_ms);
 	}
 }
 
-static void error_logger_thread(void *, void *, void *)
+static void application_thread(void *, void *, void *)
 {
-	while (true) {
-		struct sensor_fault fault;
-		int ret = k_msgq_get(&error_queue, &fault, K_FOREVER);
+	const struct zbus_observer *sub = &app_sub;
+	const struct zbus_channel *chan = NULL;
+	struct time_message msg;
+	int64_t last_epoch_ms = -1;
 
-		if (ret != 0) {
-			LOG_ERR("Falha ao ler fila de erro (%d)", ret);
+	k_thread_name_set(k_current_get(), "app");
+
+	while (zbus_sub_wait_msg(sub, &chan, &msg, K_FOREVER) == 0) {
+		if (chan != &time_channel) {
 			continue;
 		}
 
-		const char *direction =
-			(fault.error == SENSOR_ERR_TOO_LOW) ? "abaixo" :
-							      "acima";
+		if (last_epoch_ms >= 0) {
+			const int64_t delta_ms = msg.epoch_ms - last_epoch_ms;
 
-		LOG_WRN("%s inconsistente (%s do esperado): valor=%d, faixa [%d, %d], t=%lld ms",
-			sensor_names[fault.sample.type], direction,
-			fault.sample.value, fault.min_expected,
-			fault.max_expected, fault.sample.timestamp_ms);
+			LOG_INF("[App] Tempo desde ultima sincronizacao: "
+				"%lld ms", delta_ms);
+		} else {
+			LOG_INF("[App] Primeira sincronizacao recebida");
+		}
+
+		last_epoch_ms = msg.epoch_ms;
 	}
 }
 
-K_THREAD_DEFINE(temp_producer_id, PRODUCER_STACK_SIZE, producer_thread,
-		(void *)temperature_pattern, INT_TO_POINTER(SENSOR_TEMPERATURE),
-		NULL, PRODUCER_PRIORITY, 0, 0);
-K_THREAD_DEFINE(hum_producer_id, PRODUCER_STACK_SIZE, producer_thread,
-		(void *)humidity_pattern, INT_TO_POINTER(SENSOR_HUMIDITY), NULL,
-		PRODUCER_PRIORITY, 0, 0);
-K_THREAD_DEFINE(filter_id, FILTER_STACK_SIZE, filter_thread, NULL, NULL, NULL,
-		FILTER_PRIORITY, 0, 0);
-K_THREAD_DEFINE(consumer_id, CONSUMER_STACK_SIZE, consumer_thread, NULL, NULL,
-		NULL, CONSUMER_PRIORITY, 0, 0);
-K_THREAD_DEFINE(logger_id, LOGGER_STACK_SIZE, error_logger_thread, NULL, NULL,
-		NULL, LOGGER_PRIORITY, 0, 0);
+K_THREAD_DEFINE(sntp_thread_id, SNTP_STACK_SIZE, sntp_client_thread, NULL,
+		NULL, NULL, SNTP_THREAD_PRIORITY, 0, 0);
+K_THREAD_DEFINE(logger_thread_id, LISTENER_STACK_SIZE, logger_thread, NULL,
+		NULL, NULL, LOGGER_THREAD_PRIORITY, 0, 0);
+K_THREAD_DEFINE(app_thread_id, APPLICATION_STACK_SIZE, application_thread,
+		NULL, NULL, NULL, APPLICATION_THREAD_PRIORITY, 0, 0);
 
 int main(void)
 {
-	LOG_INF("Sistema de estufa inteligente inicializado");
+	LOG_INF("Sistema de sincronizacao SNTP iniciado");
 
-	/* A thread main permanece viva apenas como guardião */
 	while (true) {
 		k_sleep(K_FOREVER);
 	}
